ce6b8f84760cee60ef2d7142a10cd2570422f47f
Sergio Sanchez <sheko1999@live.com.mx>
2024-09-21 00:11:58 -0600

    modified router

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Routing from './routing.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routing />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
